774650874082b448b524e6fe9736556623ab5861
name <username.qwerty222@gmail.com>
2023-02-14 14:25:48 +0100

    Remove build image and test container

--- terraform/modules/docker_images/main.tf
+++ terraform/modules/docker_images/main.tf
@@ -1,11 +1,5 @@
 resource "docker_image" "common" {
     name = var.image_name
-
-    build {
-      context = var.build_context
-    }
-
-    keep_locally = false
 }
 
 
--- terraform/modules/docker_images/variables.tf
+++ terraform/modules/docker_images/variables.tf
@@ -2,8 +2,3 @@ variable "image_name" {
     type    = string
     default = null
 }
-
-variable "build_context" {
-    type    = string
-    default = null
-}
