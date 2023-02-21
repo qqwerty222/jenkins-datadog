e2c787627da73b6b85acfa4dac0caf4705e8fafa
name <username.qwerty222@gmail.com>
2023-02-21 12:59:35 +0100

    Set default null to secret values

--- terraform/live/variables.tf
+++ terraform/live/variables.tf
@@ -1,11 +1,14 @@
 variable "datadog_api_key" {
     type      = string
+    default   = null
     sensitive = true
 }
 
 variable "datadog_app_key" {
     type      = string
+    default   = null
     sensitive = true
+    
 }
 
 variable "api_url" {
