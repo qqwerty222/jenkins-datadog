4d30c28d6226c58ed14bf29b31c4dbe9288c66a0
name <username.qwerty222@gmail.com>
2023-02-22 11:42:12 +0100

    Uncomment docker modules

--- terraform/live/project_docker.tf
+++ terraform/live/project_docker.tf
@@ -1,78 +1,78 @@
-# module "website_net" {
-#     source = "../modules/docker/docker_network"
+module "website_net" {
+    source = "../modules/docker/docker_network"
 
-#     network_name = "website_net"
+    network_name = "website_net"
 
-#     ipam_config  = [ 
-#         # [ subnet, ip_range, gateway ]
-#         [ "172.1.0.0/16", "172.1.1.0/24", "172.1.1.254"]
-#     ]
-# }
+    ipam_config  = [ 
+        # [ subnet, ip_range, gateway ]
+        [ "172.1.0.0/16", "172.1.1.0/24", "172.1.1.254"]
+    ]
+}
 
-# module "website_node" {
-#     source = "../modules/docker/docker_container"
+module "website_node" {
+    source = "../modules/docker/docker_container"
     
-#     container_names = ["website_node1", "website_node2", "website_node3"]
-#     docker_image    = module.website_image.id
+    container_names = ["website_node1", "website_node2", "website_node3"]
+    docker_image    = module.website_image.id
 
-#     network_name    = "website_net"
-#     ipv4_address    = ["172.1.1.10", "172.1.1.11", "172.1.1.12"]
+    network_name    = "website_net"
+    ipv4_address    = ["172.1.1.10", "172.1.1.11", "172.1.1.12"]
 
-#     ports       = [
-#         # [ internal, external]
-#         [8000, null]
-#     ]
+    ports       = [
+        # [ internal, external]
+        [8000, null]
+    ]
 
-#     volumes     = [ 
-#         # [ "/host_path", "/container_path"]
-#         ["/srv/website_logs/gunicorn/access.log", "/usr/src/app/log/access.log"],
-#         ["/srv/website_logs/gunicorn/error.log",  "/usr/src/app/log/error.log"]
-#     ]
+    volumes     = [ 
+        # [ "/host_path", "/container_path"]
+        ["/srv/website_logs/gunicorn/access.log", "/usr/src/app/log/access.log"],
+        ["/srv/website_logs/gunicorn/error.log",  "/usr/src/app/log/error.log"]
+    ]
 
-#     entrypoint  = [
-#         "gunicorn",  
-#             "--bind",           "0.0.0.0:8000", 
-#             "--error-logfile",  "log/error.log",
-#             "--access-logfile", "log/access.log",
-#         "wsgi:app"
-#     ]
+    entrypoint  = [
+        "gunicorn",  
+            "--bind",           "0.0.0.0:8000", 
+            "--error-logfile",  "log/error.log",
+            "--access-logfile", "log/access.log",
+        "wsgi:app"
+    ]
 
-#     depends_on  = [ module.website_net ]
-# }
+    depends_on  = [ module.website_net ]
+}
 
-# module "nginx_node" {
-#     source = "../modules/docker/docker_container"
+module "nginx_node" {
+    source = "../modules/docker/docker_container"
 
-#     docker_image   = module.nginx_image.id
-#     container_names = ["nginx_node"]
+    docker_image   = module.nginx_image.id
+    container_names = ["nginx_node"]
     
-#     network_name    = "website_net"
-#     ipv4_address    = ["172.1.1.15"]
+    network_name    = "website_net"
+    ipv4_address    = ["172.1.1.15"]
 
-#     ports = [
-#         # [ internal, external]
-#         [80, 80]
-#     ]
+    ports = [
+        # [ internal, external]
+        [80, 80]
+    ]
 
-#     volumes = [
-#         # [ "/host_path", "/container_path"]
-#         ["${path.cwd}/../../nginx.conf", "/etc/nginx/nginx.conf"],
-#         ["/srv/website_logs/nginx/access.log", "/var/log/nginx/access.log"],
-#         ["/srv/website_logs/nginx/error.log",  "/var/log/nginx/error.log"]
-#     ]
+    volumes = [
+        # [ "/host_path", "/container_path"]
+        ["${path.cwd}/../../nginx.conf", "/etc/nginx/nginx.conf"],
+        ["/srv/website_logs/nginx/access.log", "/var/log/nginx/access.log"],
+        ["/srv/website_logs/nginx/error.log",  "/var/log/nginx/error.log"]
+    ]
 
-#     depends_on = [ module.website_net ]
-# }
+    depends_on = [ module.website_net ]
+}
 
-# module "website_image" {
-#     source = "../modules/docker/docker_images"
+module "website_image" {
+    source = "../modules/docker/docker_images"
     
-#     image_name = "localhost:5005/website"
-# }
+    image_name = "localhost:5005/website"
+}
 
-# module "nginx_image" {
-#     source = "../modules/docker/docker_images"
+module "nginx_image" {
+    source = "../modules/docker/docker_images"
 
-#     image_name   = "nginx:1.22"
-#     keep_locally = true
-# }
+    image_name   = "nginx:1.22"
+    keep_locally = true
+}
