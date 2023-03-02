d62c01db84b0d92a19a452b4a3944cb594d9f2aa
name <username.qwerty222@gmail.com>
2023-03-02 13:01:27 +0100

    Rewrite custom checks with datadog modules

--- terraform/modules/datadog/dd_custom_checks/checks.d/custom_nginx_ping.py
+++ terraform/modules/datadog/dd_custom_checks/checks.d/custom_nginx_ping.py
@@ -1,16 +1,16 @@
 from checks import AgentCheck
-import subprocess
+from datadog_checks.base.utils.subprocess_output import get_subprocess_output
 
 class NginxCheck(AgentCheck):
     def check(self, instance):
 
         # get ping result as a string
-        value = (subprocess.run(["ping -c1 172.1.1.5"], shell=True, text=True, capture_output=True).stdout)
+        out, err, retcode = get_subprocess_output(["ping", "-c1", "172.1.1.5"], self.log, raise_on_empty_output=True)
 
         # split by rows and get "time=..." from second row
-        value = (((value.split("\n"))[1]).split(" "))[-2] 
+        out = (((out.split("\n"))[1]).split(" "))[-2] 
 
         # split "time=..." and get number
-        ping_avg = float((value.split('='))[1])
+        ping_avg = float((out.split('='))[1])
 
         self.gauge('custom.nginx.ping.avg', ping_avg)
