5e9dbd7aee84c6fab482bb4adba2ea98ae35e15e
name <username.qwerty222@gmail.com>
2023-03-02 13:24:45 +0100

    Update website availability dd custom check

--- terraform/modules/datadog/dd_custom_checks/checks.d/custom_website_availability.py
+++ terraform/modules/datadog/dd_custom_checks/checks.d/custom_website_availability.py
@@ -1,20 +1,26 @@
 from checks import AgentCheck
-import subprocess
+from datadog_checks.base.utils.subprocess_output import get_subprocess_output
 import os 
 
 class NginxCheck(AgentCheck):
     def check(self, instance):
 
+        # get count containers with website
         website_node_count = int(os.environ['WEBSITE_COUNT'])
-        # website container ip range starts from 172.1.1.10 and last number in [-] included
+        
+        # set range for curl, website container get ips starting from 172.1.1.10 (172.1.1.11, 172.1.1.12 ...)
         ip_range = f"[10-{website_node_count + 9}]"
 
-        all_headers = subprocess.run([f"curl -I 'http://172.1.1.{ip_range}:8000'"], shell=True, capture_output=True, text=True).stdout
-        each_headers = all_headers.split("--_curl_--")
+        # get output from curl -I on each of conteiners
+        out, err, retcode = get_subprocess_output(["curl", "-I", f"'http://172.1.1.{ip_range}:8000'"], self.log, raise_on_empty_output=True)
+        
+        # add output for each container in list
+        each_headers = out.split("--_curl_--")
 
         status_common = []
 
-        for rows in each_headers[1:website_node_count + 1]:
+        # parse output of curl -I and save status codes in list
+        for rows in each_headers[1:]:
             status_header = (rows.split("\n"))[1]
             try: 
                 status_code = (status_header.split(" "))[1] 
@@ -26,5 +32,6 @@ class NginxCheck(AgentCheck):
             else:
                 status_common.append(0)
 
+        # send each status code with name of the container
         for index, status in enumerate(status_common):
             self.gauge(f'custom.website_node{index + 1}.availability', status)
